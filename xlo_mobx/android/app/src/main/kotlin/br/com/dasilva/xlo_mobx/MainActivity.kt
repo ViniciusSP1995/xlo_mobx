23fa95a865ee31ed12363b4391826fa374bc8e30
ViniciusSP1995 <vds1995sp@gmail.com>
2021-10-14 18:02:39 -0300

    cadastro e login

---
+++ xlo_mobx/android/app/src/main/kotlin/br/com/dasilva/xlo_mobx/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.dasilva.xlo_mobx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ xlo_mobx/lib/screens/base/base_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+import 'package:xlo_mobx/screens/home/home_screen.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
+import 'package:get_it/get_it.dart';
+
+class BaseScreen extends StatefulWidget {
+
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  final PageController pageController = PageController();
+
+  final PageStore pageStore = GetIt.I<PageStore>();
+
+  @override 
+  void initState() {
+    super.initState();
+
+    reaction(
+      (_) => pageStore.page,
+      (page) => pageController.jumpToPage(page)
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          HomeScreen(),
+          Container(color: Colors.green),
+          Container(color: Colors.yellow),
+          Container(color: Colors.purple),
+          Container(color: Colors.brown),
+        ]
+      )
+
+    );
+  }
+}
